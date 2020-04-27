b1393cb3e699acb4ba7c8e595703557de59ec4b2
Kishor Naik <kishor.naik011.net@gmail.com>
2020-04-27 13:37:56 +0530

    Code Commit

---
+++ Sol_Demo/Sol_Demo/Pages/RatingComponents/RatingJsExtension.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sol_Demo.Pages.RatingComponents
+{
+    public static class RatingJsExtension
+    {
+        public static async Task RatingBlazorJsAsync(this ElementReference elementReference, IJSRuntime jsRuntime, string ratingJsonData)
+        {
+            await jsRuntime.InvokeVoidAsync(
+                    "ratingBlazorDemo", elementReference, ratingJsonData);
+        }
+    }
+}
---
+++ Sol_Demo/Sol_Demo/wwwroot/js/ratingBlazor.js
@@ -0,0 +1,19 @@
+﻿function ratingBlazorDemo(ratingElement, ratingJsonData) {
+
+    console.log(ratingJsonData);
+
+    // Parse Json Data
+    let ratingObject = JSON.parse(ratingJsonData);
+    console.log(ratingObject);
+
+    $('.starrr').starrr({
+        rating: ratingObject.rating,
+        max: ratingObject.max,
+        change: function (e, value) {
+            console.log('new rating is ' + value);
+
+            DotNet.invokeMethodAsync('Sol_Demo', 'OnSelectedRatingJS', String(value));
+        }
+    });
+
+}
